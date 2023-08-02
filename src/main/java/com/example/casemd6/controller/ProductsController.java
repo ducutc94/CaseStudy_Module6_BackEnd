0f5da8716067045cc3a3db46413ad08f715cde02
Phamquangduong09 <Phamquangduong009@gmail.com>
2023-08-02 15:00:49 +0700

    done CRUD Products

---
+++ src/main/java/com/example/casemd6/controller/ProductsController.java
@@ -0,0 +1,57 @@
+package com.example.casemd6.controller;
+
+
+import com.example.casemd6.model.Products;
+import com.example.casemd6.service.IProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/products")
+public class ProductsController {
+    @Autowired
+    private IProductsService productService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Products>> showProductAll() {
+        return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Products>> findOneProduct(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.findOne(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Products> createProducts(@RequestBody Products products) {
+        productService.save(products);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Products> updateProduct(@PathVariable Long id, @RequestBody Products p) {
+        Optional<Products> studentOptional = productService.findOne(id);
+        if (!studentOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        p.setId(id);
+        productService.save(p);
+        return new ResponseEntity<>(p, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Optional<Products>> deleteProduct(@PathVariable Long id) {
+        Optional<Products> student = productService.findOne(id);
+        if (student.isPresent()) {
+            productService.remove(id);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
--- src/main/java/com/example/casemd6/repository/IProductsRepository.java
+++ src/main/java/com/example/casemd6/repository/IProductsRepository.java
@@ -2,8 +2,15 @@ package com.example.casemd6.repository;
 
 import com.example.casemd6.model.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IProductsRepository extends JpaRepository<Products,Long> {
+
+
+    @Query(value = "SELECT * FROM products WHERE status_products = 0", nativeQuery = true)
+    List<Products> findProductsByStatusZero();
 }
