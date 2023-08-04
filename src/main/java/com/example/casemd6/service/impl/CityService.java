d2271239eb31deb0dc32f12386f05892f13fed11
TuanVu0929 <tranngoc.haidang0402@gmail.com>
2023-08-04 09:54:33 +0700

    tao city

--- src/main/java/com/example/casemd6/service/impl/CityService.java
+++ src/main/java/com/example/casemd6/service/impl/CityService.java
@@ -1,16 +1,20 @@
 package com.example.casemd6.service.impl;
 
 import com.example.casemd6.model.City;
+import com.example.casemd6.repository.ICityRepository;
 import com.example.casemd6.service.ICityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class CityService implements ICityService {
+    @Autowired
+    private ICityRepository iCityRepository;
     @Override
     public Iterable<City> findAll() {
-        return null;
+        return iCityRepository.findAll();
     }
 
     @Override
