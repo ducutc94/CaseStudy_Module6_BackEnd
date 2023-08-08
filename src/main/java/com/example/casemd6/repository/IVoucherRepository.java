1dcd8717905b7d0f03ebaa66e445bead8464cc77
TuanVu0929 <tranngoc.haidang0402@gmail.com>
2023-08-08 16:32:23 +0700

    CRUD Voucher

--- src/main/java/com/example/casemd6/repository/IVoucherRepository.java
+++ src/main/java/com/example/casemd6/repository/IVoucherRepository.java
@@ -2,8 +2,14 @@ package com.example.casemd6.repository;
 
 import com.example.casemd6.model.Voucher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Repository
 public interface IVoucherRepository extends JpaRepository<Voucher,Long> {
+    @Query(value = "select * from voucher inner join voucher_shops vs on voucher.id = vs.voucher_id  and shops_id = ? where status_voucher = '0';",nativeQuery = true)
+    List<Voucher> findAllByShop(@PathVariable Long id);
 }
--- src/main/java/com/example/casemd6/service/impl/VoucherService.java
+++ src/main/java/com/example/casemd6/service/impl/VoucherService.java
@@ -1,30 +1,45 @@
 package com.example.casemd6.service.impl;
 
 import com.example.casemd6.model.Voucher;
+import com.example.casemd6.repository.IVoucherRepository;
 import com.example.casemd6.service.IVoucherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class VoucherService implements IVoucherService {
+    @Autowired
+    private IVoucherRepository iVoucherRepository;
     @Override
     public Iterable<Voucher> findAll() {
-        return null;
+        return iVoucherRepository.findAll();
+    }
+
+    @Override
+    public List<Voucher> findAllByShop(Long id) {
+        return iVoucherRepository.findAllByShop(id);
     }
 
     @Override
     public Optional<Voucher> findOne(Long id) {
-        return Optional.empty();
+        return iVoucherRepository.findById(id);
     }
 
     @Override
     public Voucher save(Voucher voucher) {
-        return null;
+        return iVoucherRepository.save(voucher);
     }
+
 
     @Override
     public void remove(Long id) {
-
+        Optional<Voucher> voucherOptional = iVoucherRepository.findById(id);
+        if (voucherOptional.isPresent()){
+            voucherOptional.get().setStatusVoucher("1");
+            iVoucherRepository.save(voucherOptional.get());
+        }
     }
 }
